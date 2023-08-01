1258695f9fd7a971b0f13d7741b2599ec69c348b
Kwon Min A <min2010011@gmail.com>
2023-08-01 17:56:50 +0900

    [BE] chore: 프로젝트 세팅 #13

---
+++ backend/src/main/java/com/softeer/caart/CaartServerApplication.java
@@ -0,0 +1,13 @@
+package com.softeer.caart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaartServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaartServerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/softeer/caart/CaartServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softeer.caart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaartServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
