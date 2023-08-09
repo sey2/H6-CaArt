35e7c7b0c96a8e805f98dfadcfa8074d993ae874
tommya98 <63833290+tommya98@users.noreply.github.com>
2023-08-09 18:41:03 +0900

    [FE] feat: 라이프스타일 카드 컴포넌트 구현 (#178)
    * [FE] feat: 라이프스타일 카드 컴포넌트 구현 #165
    
    * [FE] feat: 라이프스타일 카드 설명 개행 적용 #165

---
+++ frontend/src/components/recommendPage/lifeStyleCard/LifeStyleCard.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import styled from 'styled-components';
+import CheckButton from '../../common/CheckButton';
+import { TagList } from '../../common/TagList';
+
+function LifeStyleCard({
+  id,
+  tag,
+  text,
+  imgSrc,
+  selected,
+}: LifeStyleCardProps & LifeStyleSelectedProps) {
+  return (
+    <LifeStyleCardBox selected={selected}>
+      <LifeStyleImgBox src={imgSrc} selected={selected}></LifeStyleImgBox>
+      <LifeStyleInnerBox>
+        <TagList tagArr={tag} type={'lifeStyle'} selected={selected}></TagList>
+        <LifeStyleTextBox selected={selected}>
+          <div className="body-medium-18 text-primary-blue">{text}</div>
+          <ClickDivBox
+            onClick={() => {
+              console.log(id);
+            }}
+          >
+            <CheckButton selected={selected}></CheckButton>
+          </ClickDivBox>
+        </LifeStyleTextBox>
+        <LifeStylePeekBox>
+          <ClickDivBox
+            className="body-medium-14 text-grey-200"
+            onClick={() => {
+              console.log(id);
+            }}
+          >
+            라이프스타일 엿보기
+          </ClickDivBox>
+        </LifeStylePeekBox>
+      </LifeStyleInnerBox>
+    </LifeStyleCardBox>
+  );
+}
+
+interface LifeStyleCardProps {
+  id: number;
+  tag: string[];
+  text: string;
+  imgSrc: string;
+}
+
+interface LifeStyleSelectedProps {
+  selected?: boolean;
+}
+
+const LifeStyleCardBox = styled.div<LifeStyleSelectedProps>`
+  display: flex;
+  align-items: flex-start;
+  width: 296px;
+  height: 180px;
+  border-radius: 8px;
+  background: ${props =>
+    props.selected ? `var(--grey-1000)` : `var(--primary-blue-10)`};
+  border: ${props => (props.selected ? `1.5px solid var(--primary-blue)` : ``)};
+`;
+
+const LifeStyleImgBox = styled.img<LifeStyleSelectedProps>`
+  position: absolute;
+  top: -40px;
+  left: 188px;
+  width: 88px;
+  height: 88px;
+  border-radius: 50%;
+  border: ${props =>
+    props.selected
+      ? `1.5px solid var(--primary-blue)`
+      : `1.5px solid var(--primary-blue-10)`};
+`;
+
+const LifeStyleInnerBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 20px 20px 16px 20px;
+`;
+
+const LifeStyleTextBox = styled.div<LifeStyleSelectedProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 256px;
+  padding-top: 10px;
+  padding-bottom: 20px;
+  margin-bottom: 10px;
+  border-bottom: ${props =>
+    props.selected ? `1px solid var(--grey-700)` : `1px solid var(--grey-900)`};
+
+  div {
+    white-space: pre-wrap;
+  }
+`;
+
+const ClickDivBox = styled.div`
+  cursor: pointer;
+`;
+
+const LifeStylePeekBox = styled.div`
+  padding-left: 73px;
+`;
+
+export { LifeStyleCard };
