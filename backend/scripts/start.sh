d846f3eb2dc296a57bd4c506101c8c61f2ca31d8
Kwon Min A <71026706+mingulmangul@users.noreply.github.com>
2023-08-09 13:23:37 +0900

    [BE] fix: CD workflow 수정 (#150)
    * [BE] chore: 빌드 시 plain.jar 생성하지 않도록 변경 #121
    
    * [BE] feat: 애플리케이션 실행 스크립트 추가 #121
    
    * [BE] feat: 배포 워크플로 수정 #121
    
    CodeDeploy 추가
    
    * [BE] feat: working directory를 /backend로 설정 #121
    
    * [BE] chore: yaml 파일 .gitignore에서 제외 #121
    
    applicatoin.yml 파일만 .gitignore에 설정
    
    * [BE] feat: CodeDeploy 설정 파일(appspec.yml) 추가 #121
    
    * [BE] test: 테스트용 워크플로 트리거 제거 #121
    
    BE/fix/cd 브랜치에 대한 트리거 제거

---
+++ backend/scripts/start.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ubuntu/caart"
+JAR_FILE="$PROJECT_ROOT/caart-webapp.jar"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 실행 중인 애플리케이션이 없습니다." >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 실행 중인 $CURRENT_PID 애플리케이션을 종료합니다." >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+fi
+
+echo "$TIME_NOW > $JAR_FILE 파일 생성" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
