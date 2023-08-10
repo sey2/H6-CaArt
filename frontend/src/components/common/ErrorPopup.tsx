7fe010f40dbb9543f8bebd512bf26eb7e6144a82
tommya98 <63833290+tommya98@users.noreply.github.com>
2023-08-10 22:43:56 +0900

    [FE] API요청을 위한 useFetch 커스텀 훅 구현 (#202)
    * [FE] API요청을 위한 useFetch 훅 구현 #181
    
    * [FE] feat: 오류 팝업 구현 #181
    
    * [FE] fix: base URL 상수화 #181

---
+++ frontend/src/components/common/ErrorPopup.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import SquareButton from './SquareButton';
+
+function ErrorPopup() {
+  useEffect(() => {
+    document.body.style.cssText = `
+      background: rgba(15,17,20,0.55);`;
+  }, []);
+
+  return (
+    <ErrorPopupBox>
+      <ErrorPopupText className="head-medium-22 text-grey-50">
+        내 차 만들기 과정 중 <br />
+        오류가 발생했어요
+      </ErrorPopupText>
+      <ErrorPopupImg src="/images/error_logo_blue.svg"></ErrorPopupImg>
+      <SquareButton
+        size="l"
+        color="grey-1000"
+        bg="primary-blue"
+        onClick={() => {
+          location.reload();
+        }}
+      >
+        확인
+      </SquareButton>
+    </ErrorPopupBox>
+  );
+}
+
+const ErrorPopupBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 427px;
+  height: 392px;
+  padding: 24px 32px 32px 32px;
+  border-radius: 12px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: var(--grey-1000);
+`;
+
+const ErrorPopupText = styled.div``;
+
+const ErrorPopupImg = styled.img`
+  width: 222px;
+  height: 138px;
+  margin-top: 48px;
+  margin-bottom: 48px;
+  transform: translateX(70px);
+`;
+
+export { ErrorPopup };
---
+++ frontend/src/hooks/useFetch.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+type FetchStatus = 'idle' | 'loading' | 'success' | 'error';
+
+interface FetchResponse<T> {
+  data: T | null;
+  error: string | null;
+  status: FetchStatus;
+}
+
+const BASE_URL = 'http://api.ca-art.store';
+
+const useFetch = <T>(url: string): FetchResponse<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [status, setStatus] = useState<FetchStatus>('idle');
+
+  useEffect(() => {
+    if (!url) return;
+
+    const fetchData = async () => {
+      setStatus('loading');
+      try {
+        const response = await fetch(`${BASE_URL}${url}`);
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+        setStatus('success');
+      } catch (error) {
+        setError(String(error));
+        setStatus('error');
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, status, error };
+};
+
+export { useFetch };
