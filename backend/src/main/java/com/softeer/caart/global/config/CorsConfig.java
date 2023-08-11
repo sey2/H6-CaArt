44ce885f6566b9f13624eae97aa63c35b8153f5a
규규 <62997391+rbgus2002@users.noreply.github.com>
2023-08-11 22:35:00 +0900

    [BE] fix: cors 설정 추가 #219 (#220)
    - "http://api.ca-art.store" 허용
    - "http://localhost:3000" 허용 (front)

---
+++ backend/src/main/java/com/softeer/caart/global/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.softeer.caart.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://api.ca-art.store/", "http://localhost:3000")
+			.allowedMethods("GET", "POST")
+			.maxAge(3000);
+	}
+}
