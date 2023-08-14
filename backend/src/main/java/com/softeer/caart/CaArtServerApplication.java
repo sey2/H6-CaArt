12e80acdcbacbe1d28a3bd86a33bcad7349da06c
규규 <62997391+rbgus2002@users.noreply.github.com>
2023-08-14 13:23:50 +0900

    [BE] feat: logging 전략 수립 (#239)
    * [BE] refactor: ExceptionHandler 추가 및 Exception 로그 메세지 수정 #235
    
    - UnexpectedTypeException 예외 처리 추가
    - DataIntegrityViolationException 예외 처리 추가
    
    * [BE] chore: sentry 의존성 추가 #235
    
    * [BE] feat: reflection을 사용한 service, controller 계층 별로 로그 찍히도록 구현 #235

--- backend/src/main/java/com/softeer/caart/CaArtServerApplication.java
+++ backend/src/main/java/com/softeer/caart/CaArtServerApplication.java
@@ -3,7 +3,10 @@ package com.softeer.caart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class CaArtServerApplication {
 
 	public static void main(String[] args) {
