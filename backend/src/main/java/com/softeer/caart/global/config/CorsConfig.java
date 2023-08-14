901c751a5ec17d64379cea741d148f37a49e0c19
Kwon Min A <71026706+mingulmangul@users.noreply.github.com>
2023-08-14 16:11:48 +0900

    [BE] feat: HTTPS 연결 (#261)
    * [BE] feat: 배포 워크플로 트리거에 BE/deploy/** 브랜치 추가 #254
    
    * [BE] feat: swagger api 서버 주소를 운영과 로컬로 분리 #254
    
    * [BE] feat: CORS allowed origin 변경 #254

--- backend/src/main/java/com/softeer/caart/global/config/CorsConfig.java
+++ backend/src/main/java/com/softeer/caart/global/config/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://api.ca-art.store/", "http://localhost:3000")
+			.allowedOrigins("http://api.ca-art.store",
+				"https://api.ca-art.store",
+				"http://localhost:3000")
 			.allowedMethods("GET", "POST")
 			.maxAge(3000);
 	}
--- backend/src/main/java/com/softeer/caart/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/softeer/caart/global/config/SwaggerConfig.java
@@ -1,21 +1,28 @@
 package com.softeer.caart.global.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @Configuration
+@OpenAPIDefinition(servers = {
+	@Server(url = "https://api.ca-art.store", description = "Default server URL"),
+	@Server(url = "http://localhost:8080", description = "Local server URL for development")
+})
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI openAPI() {
-        Info info = new Info()
-                .title("CaArt API 명세서")
-                .version("v0.0.1")
-                .description("API documentation");
-        return new OpenAPI()
-                .components(new Components())
-                .info(info);
-    }
+	@Bean
+	public OpenAPI openAPI() {
+		Info info = new Info()
+			.title("CaArt API 명세서")
+			.version("v0.0.1")
+			.description("API documentation");
+		return new OpenAPI()
+			.components(new Components())
+			.info(info);
+	}
 }
