76a28c8d4b4f140aea63ba4d9b51324049bf59b7
규규 <62997391+rbgus2002@users.noreply.github.com>
2023-08-17 17:50:01 +0900

    [BE] fix: native query 수정 #321 (#322)

--- backend/src/main/java/com/softeer/caart/domain/model/repository/ModelRepository.java
+++ backend/src/main/java/com/softeer/caart/domain/model/repository/ModelRepository.java
@@ -9,6 +9,6 @@ import com.softeer.caart.domain.model.Model;
 
 public interface ModelRepository extends JpaRepository<Model, Long> {
 
-	@Query(value = "SELECT * FROM Model WHERE trim_id = ?1 AND car_engine_id = ?2 AND body_type_id = ?3 AND wd_id = ?4", nativeQuery = true)
+	@Query(value = "SELECT * FROM model WHERE trim_id = ?1 AND car_engine_id = ?2 AND body_type_id = ?3 AND wd_id = ?4", nativeQuery = true)
 	Optional<Model> findModelByTrimIdAndCompositionsId(Long trimId, Long engineId, Long bodyTypeId, Long wdId);
 }
