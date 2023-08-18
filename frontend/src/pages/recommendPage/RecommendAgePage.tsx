8073195fa3bf1da02517e43036d3ba853abba979
tommya98 <63833290+tommya98@users.noreply.github.com>
2023-08-18 15:17:03 +0900

    [FE] feat: 라이프스타일 사전 질문 API 연동 #317 (#335)

--- frontend/src/pages/recommendPage/RecommendAgePage.tsx
+++ frontend/src/pages/recommendPage/RecommendAgePage.tsx
@@ -1,62 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { ErrorPopup } from '../../components/common/ErrorPopup';
 import SquareButton from '../../components/common/SquareButton';
 import { PageNum } from '../../components/recommendPage/ageAndLifeStyle/PageNum';
 import { RecommendPageButton } from '../../components/recommendPage/ageAndLifeStyle/RecommendPageButton';
+import { useFetch } from '../../hooks/useFetch';
 import { RecommendPageProps } from './RecommendPage';
 
+interface questionProps {
+  age: {
+    question: string;
+    keyword: string;
+    choices: {
+      id: number;
+      content: string;
+    }[];
+  };
+}
+
 function RecommendAgePage({ choice, setChoice }: RecommendPageProps) {
+  const { data, status, error } = useFetch<questionProps>(
+    '/lifestyles/questions',
+  );
+  if (status === 'loading') {
+    return <div>loading</div>;
+  } else if (status === 'error') {
+    console.error(error);
+    return <ErrorPopup></ErrorPopup>;
+  }
+  if (data === null) return <div></div>;
+
+  const questionList = data.age.choices.map((item, index) => {
+    return (
+      <RecommendPageButton
+        key={item.id}
+        size={
+          data.age.choices.length % 2 == 1 &&
+          data.age.choices.length === index - 1
+            ? 'large'
+            : 'small'
+        }
+        selected={choice.age === item.id}
+        onClick={() => {
+          setChoice({ ...choice, age: item.id });
+        }}
+      >
+        {item.content}
+      </RecommendPageButton>
+    );
+  });
+
   return (
     <ReccomendAgePageBox>
       <ReccomendAgePageMain>
         <ReccomendAgePageQBox>
           <div className="text-grey-0">
-            <span className="head-medium-22 ">나이</span>
-            <span className="head-regular-22">를 알려주세요</span>
+            <span className="head-medium-22 ">{data.age.keyword}</span>
+            <span className="head-regular-22">
+              {data.age.question.replace(data.age.keyword, '')}
+            </span>
           </div>
           <PageNum>1/2</PageNum>
         </ReccomendAgePageQBox>
-
-        <ReccomendAgePageABox>
-          <RecommendPageButton
-            size="small"
-            selected={choice.age === 0}
-            onClick={() => {
-              setChoice({ ...choice, age: 0 });
-            }}
-          >
-            20대
-          </RecommendPageButton>
-          <RecommendPageButton
-            size="small"
-            selected={choice.age === 1}
-            onClick={() => {
-              setChoice({ ...choice, age: 1 });
-            }}
-          >
-            30대
-          </RecommendPageButton>
-          <RecommendPageButton
-            size="small"
-            selected={choice.age === 2}
-            onClick={() => {
-              setChoice({ ...choice, age: 2 });
-            }}
-          >
-            40대
-          </RecommendPageButton>
-          <RecommendPageButton
-            size="small"
-            selected={choice.age === 3}
-            onClick={() => {
-              setChoice({ ...choice, age: 3 });
-            }}
-          >
-            50대 이상
-          </RecommendPageButton>
-        </ReccomendAgePageABox>
-
+        <ReccomendAgePageABox>{questionList}</ReccomendAgePageABox>
         <Link to="/recommend/lifeStyle">
           <SquareButton size="xl" color="grey-1000" bg="primary-blue">
             다음
